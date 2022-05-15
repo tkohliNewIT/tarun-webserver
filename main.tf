2c790ef01dfd578f0606fe488e952e95da0ffc9d
Tarun Kohli <engg.tarun87@gmail.com>
2022-05-15 17:12:55 -0500

    Added Tags

--- main.tf
+++ main.tf
@@ -7,13 +7,17 @@ resource "aws_instance" "Prod1_EC2" {
     instance_type = "t2.micro"
 
     tags = {
-      "name" = "Prod-App1"
+      "Name" = "Prod-App1"
     }
   }
 
 resource "aws_instance" "Prod2_EC2" {
     ami = "ami-0022f774911c1d690"
     instance_type = "t2.micro"
+
+tags = {
+  "Name" = "Prod-App2"
+}
 
 }
 
